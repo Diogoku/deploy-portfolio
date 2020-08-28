afb3f327576ca93ef7ce55241f8c85f01f692986
diogoku <diogomnribeiro@gmail.com>
2020-08-29 00:53:01 +0100

    gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "index_bundle.js",
-    publicPath: "/",
+    publicPath: "/deploy-portfolio2",
   },
   module: {
     rules: [
